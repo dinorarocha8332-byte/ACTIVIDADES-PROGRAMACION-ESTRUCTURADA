b7d064dba5539742b5a6e8dc83926899fbefb034
dinorarocha8332-byte <dinora.rocha8332@alumnos.udg.mx>
2026-02-13 08:44:35 -0600

    Add files via upload

---
+++ actividades viernes 13 correctas ahora si/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int num;
+    int i;
+    int esPrimo = 1;
+
+    printf("Ingresa un numero: ");
+    scanf("%d", &num);
+
+    if (num % 2 == 0) {
+        printf("El numero es PAR\n");
+    } else {
+        printf("El numero es IMPAR\n");
+    }
+
+    if (num <= 1) {
+        esPrimo = 0;
+    } else {
+        i = 2;
+        while (i <= num / 2 && esPrimo == 1) {
+            if (num % i == 0) {
+                esPrimo = 0;
+            }
+            i++;
+        }
+    }
+
+    if (esPrimo == 1) {
+        printf("El numero es PRIMO\n");
+    } else {
+        printf("El numero NO es primo\n");
+    }
+
+    return 0;
+}
